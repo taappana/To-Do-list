e52791c0c79b4fd93d812f42dbefa7a5cb2fd0e4
Jani <herranen.jani@gmail.com>
2017-03-26 21:22:59 +0300

    finished folder structure, added the html and some css and js

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+//Check off specific todos by clicking
+$("li").click(function() {
+  $(this).toggleClass("completed");
+});
+
+/*//Check off specific todos by clicking
+$("li").click(function() {
+  //if li is gray
+  if($(this).css("color") === "rgb(128, 128, 128)"){
+    //turn it black
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  }
+  //else
+  else {
+    //turn it gray
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
+  }
+});*/
