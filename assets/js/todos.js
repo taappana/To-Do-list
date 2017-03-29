9de949f13ba6c06952a8da38cafff95026c0b0ca
Jani <herranen.jani@gmail.com>
2017-03-29 18:24:58 +0300

    Finished up

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,24 +1,23 @@
 //Check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
-/*//Check off specific todos by clicking
-$("li").click(function() {
-  //if li is gray
-  if($(this).css("color") === "rgb(128, 128, 128)"){
-    //turn it black
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    });
+$("ul").on("click", "span", function(e){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+  if(e.which === 13){
+    var toDoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + toDoText + "</li>");
   }
-  //else
-  else {
-    //turn it gray
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    });
-  }
-});*/
+});
+
+$(".fa-plus-circle").click(function(){
+  $("input[type='text']").fadeToggle();
+});
